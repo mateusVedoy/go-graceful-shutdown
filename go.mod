172c381631d8f9e794a30666b185a3c2b1ae3c89
mateusVedoy <mateusvedoy@outlook.com>
2024-06-06 09:38:02 -0300

    feat: implements first shutdown treatment

---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/golesson/go-graceful-shutdown
+
+go 1.20
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/go-chi/render v1.0.3
+)
+
+require (
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+)
+
+require (
+	github.com/ajg/form v1.5.1 // indirect
+	github.com/unrolled/render v1.6.1
+)
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(time.Second * 3)
+		w.Write([]byte("Hello"))
+	})
+
+	server := &http.Server{Addr: ":8080"}
+
+	go func() {
+		if er := server.ListenAndServe(); er != nil && http.ErrServerClosed != er {
+			panic(er)
+		}
+	}()
+
+	stop := make(chan os.Signal)
+	signal.Notify(stop, syscall.SIGTERM, os.Interrupt)
+	<-stop
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("stopped")
+
+}
