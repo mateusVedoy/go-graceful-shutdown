e8f99fdaa4aaf987255676e0c3fa2973cfe9e0e3
mateusVedoy <mateusvedoy@outlook.com>
2024-06-10 11:11:24 -0300

    refact: main now can use two different graceful shutdown strategy

--- main.go
+++ main.go
@@ -1,41 +1,13 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
+	"github.com/golesson/go-graceful-shutdown/tests"
 )
 
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(time.Second * 3)
-		w.Write([]byte("Hello"))
-	})
+	//test with first way to achieve it
+	// tests.With_native_libs()
 
-	server := &http.Server{Addr: ":8080"}
-
-	go func() {
-		if er := server.ListenAndServe(); er != nil && http.ErrServerClosed != er {
-			panic(er)
-		}
-	}()
-
-	stop := make(chan os.Signal)
-	signal.Notify(stop, syscall.SIGTERM, os.Interrupt)
-	<-stop
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
-	defer cancel()
-
-	if err := server.Shutdown(ctx); err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("stopped")
-
+	tests.With_go_chi()
 }
